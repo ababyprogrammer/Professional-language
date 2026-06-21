d32513fc7e8d50918f1c81f3a32e234bf2802c55
A baby programmer <139476654+ababyprogrammer@users.noreply.github.com>
2026-06-21 21:42:50 +0330

    Update Compute quotient/remainder.cpp

--- C++/Compute quotient and remainder.cpp
+++ C++/Compute quotient and remainder.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
+      int divisor, dividend, quotient, remainder;
 
+    cout << "Enter dividend: ";
+    cin >> dividend;
+
+    cout << "Enter divisor: ";
+    cin >> divisor;
+
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+
+    cout << "Quotient = " << quotient << endl;
+    cout << "Remainder = " << remainder;
+
+    return 0;
 }
